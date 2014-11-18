8731dd1bc27dc50b05ecb3190a3c056448e99704
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-19 02:24:23 +0900

    Add Initializers Step

--- Struct.playground/section-1.swift
+++ Struct.playground/section-1.swift
@@ -18,3 +18,19 @@ struct Contact2 {
 }
 
 var person2 = Contact2(firstName: "Jon", lastName: "Smith", type: "Friend")
+
+// Initializers
+
+struct Contact3 {
+	let firstName: String
+	let lastName: String
+	var type: String
+
+	init(fName: String, lName: String) {
+		self.firstName = fName
+		self.lastName = lName
+		self.type = "Friend"
+	}
+}
+
+var person3 = Contact3(fName: "Jon", lName: "Smith")
