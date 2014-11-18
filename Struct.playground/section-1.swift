556d5cfea54368ca21a73a47af10e8bbba709ad0
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-19 01:56:03 +0900

    Add Default Values Step

--- Struct.playground/section-1.swift
+++ Struct.playground/section-1.swift
@@ -10,3 +10,11 @@ struct Contact {
 var person = Contact(firstName: "Jon", lastName: "Smith")
 person.firstName  = "Tom"
 person.lastName
+
+struct Contact2 {
+	let firstName: String
+	let lastName: String
+	var type: String = "Friend"
+}
+
+var person2 = Contact2(firstName: "Jon", lastName: "Smith", type: "Friend")
