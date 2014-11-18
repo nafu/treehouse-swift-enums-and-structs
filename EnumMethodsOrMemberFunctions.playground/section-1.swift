c6eefb327f5f1e0898452cf29879549fd7c27060
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-19 01:42:05 +0900

    Add Initializer Step

--- EnumMethodsOrMemberFunctions.playground/section-1.swift
+++ EnumMethodsOrMemberFunctions.playground/section-1.swift
@@ -1,12 +1,26 @@
 // Enum Methods or Member Functions
+// Initializer
 
 import UIKit
 
 enum Day: Int {
 	case Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
 
+	init() {
+		self = .Monday
+	}
+
 	func daysTillWeekend() -> Int {
 		return Day.Saturday.rawValue - self.rawValue
+	}
+
+	func dayString() -> String {
+		switch self {
+		case .Monday:
+			return "Monday"
+		default:
+			return "Other Day"
+		}
 	}
 }
 
@@ -18,3 +32,7 @@ today.daysTillWeekend()
 
 var holiday = Day.Saturday
 holiday.daysTillWeekend()
+
+today = Day()
+today.rawValue
+today.dayString()
