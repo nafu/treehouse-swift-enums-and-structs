70f1f80efdc021417cf1edc5eda4ba3a9b7563b0
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-11-19 02:45:17 +0900

    Add Review and Final Exam Step

---
+++ Todo.playground/section-1.swift
@@ -0,0 +1,30 @@
+// Final Exam
+// Todo
+
+import UIKit
+
+////////////////////////////////////////////////////////////
+// Task:
+//		description
+//		status: Doing, Pending, Completed
+////////////////////////////////////////////////////////////
+
+enum Status {
+	case Doing, Pending, Completed
+
+	init() {
+		self = .Pending
+	}
+}
+
+struct Task {
+	var description: String
+	var status = Status()
+
+	init(description: String) {
+		self.description = description
+	}
+}
+
+var task = Task(description: "Have fun")
+task.status = .Completed
